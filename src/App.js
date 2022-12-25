e6e56e41e760d88b4967c5f5a414db8bdf1ac681
honaseung <lhs5948@gmail.com>
2022-12-26 00:46:05 +0900

    create TodoInsert and add on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <TodoTemplate>TODO APP 을 만들자!</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
 };
 
 export default App;
